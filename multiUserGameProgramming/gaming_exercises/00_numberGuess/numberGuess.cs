59017063209653cf3af057d30152351f980d42a5
A.O.Burx <amarionoburks@gmail.com>
2023-09-29 14:50:13 -0400

    Update numberGuess.cs

--- multiUserGameProgramming/gaming_exercises/00_numberGuess/numberGuess.cs
+++ multiUserGameProgramming/gaming_exercises/00_numberGuess/numberGuess.cs
@@ -1,4 +1,4 @@
-// Amarion Burks, Number Guess, v0.4
+// Amarion Burks, Number Guess, v0.5
 
 /* 
 generate random number
@@ -14,6 +14,15 @@ for difficulty, can change # of guesses or number range
 */
 
 
+
+
+
+
+
+
+
+
+
 using System;
 
 namespace numberGuess
@@ -22,7 +31,7 @@ namespace numberGuess
     {
         static void Main(string[] args)
         {
-            string userInput = "";
+            int userGuess = 0;
             int numGuesses = 3;
             int numAttempts = 0;
             int playerScore = 0;
@@ -32,9 +41,9 @@ namespace numberGuess
             int rangeMin = -1;
             int rangeMax = 100;
 
-
+            Console.WriteLine("");
             Console.WriteLine("Welcome to the Number Guessing Game!");
-            Console.WriteLine("Please select your difficulty\nEasy Mode: Range is 0 - 10 with 4 guesses. \nNormal Mode: Range is 0 - 25 with 4 guesses. \nHard Mode: Range is 0 - 50 with  guesses.");
+            Console.WriteLine("Please select your difficulty\nEasy Mode: Range is 0 - 10 with 4 guesses. \nNormal Mode: Range is 0 - 25 with 4 guesses. \nHard Mode: Range is 0 - 50 with 3 guesses.");
 
             difficulty = Console.ReadLine();
             Console.WriteLine("You have selected " + difficulty);
@@ -42,7 +51,7 @@ namespace numberGuess
             if (difficulty == "easy") {
                 numGuesses = 4;
                 rangeMin = 0;
-                rangeMax = 0;
+                rangeMax = 10;
             } else if (difficulty == "normal") {
                 numGuesses = 4;
                 rangeMin = 0;
@@ -59,23 +68,57 @@ namespace numberGuess
             
             } else {
                 //if nothing else selected
+                Console.WriteLine(difficulty + "is not a valid difficulty, selecting Easy Mode.");
                 numGuesses = 4;
                 rangeMin = 0;
-                rangeMax = 0;
+                rangeMax = 10;
             }
+            // Tell player min, max, and guesses
 
             while (playerScore != 3 && cpuScore != 3) {
                 // Any code to run BEFORE each round goes here
-                Console.WriteLine("Select a number between" + rangeMin + " and " + rangeMax)
+                // Generate secret #
+                Random rndNum = new Random();
+                curNumber = rndNum.Next(rangeMin, rangeMax);
+                Console.WriteLine(curNumber); //REMOVE AFTER TESTING
+                Console.Write("Player Score: " + playerScore + ", CPU Score: " + cpuScore + "\n");
+
                 //START EACH ROUND
                 for (int i = 0; i < numGuesses; i++) {
                     // Code to guess number goes here
-                    
+                    Console.WriteLine("You have used " + numAttempts + " attempts this round");
+                    Console.WriteLine("Select a number between " + rangeMin + " and " + rangeMax + "\nYou have " + numGuesses + " max guesses.");
+                    userGuess = System.Convert.ToInt32(Console.ReadLine());
+                    if (userGuess == curNumber) {
+                        Console.WriteLine("You got it right! You get 1 point!");
+                        playerScore++;
+                        numAttempts = 0;
+                        break;
+                    } else {
+                        if (userGuess > curNumber) {
+                            Console.WriteLine("Your guess is too high!\n");
+                        } else {
+                            Console.WriteLine("Your guess is too low!\n");
+                        }   
+                    }
+                    numAttempts++;
                 }
+                if (userGuess != curNumber) {
+                    cpuScore++;
+                    numAttempts = 0;
+                    Console.WriteLine("You lost! CPU gets a point!");
+                }                
 
             }
 
+            if (playerScore >= 3) {
+                Console.WriteLine("You won the game!\n");
+            } else {
+                Console.WriteLine("You lost the game!\n");
 
+            }
+
+            
 
 
 
