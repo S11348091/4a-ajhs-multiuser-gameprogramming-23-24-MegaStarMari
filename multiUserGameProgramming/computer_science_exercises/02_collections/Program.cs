be923c814b24897fb092b2a0e881bfd478695d3c
A.O.Burx <amarionoburks@gmail.com>
2023-10-03 14:50:34 -0400

    02_Collections v0.1a + v0.1b

--- multiUserGameProgramming/computer_science_exercises/02_collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/02_collections/Program.cs
@@ -1,4 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//02_Collections, Amarion Burks, v0.0
+//02_Collections, Amarion Burks, v0.1b
 
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace UpdateForEachProgram
+{
+    class UpdateForEachProgram
+    {
+        static void Main(string[] args)
+        {
+            // Collections are variables that can store multiple values in one variable
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change
+            -- Contents of elements in an array CAN change
+            -- Items in the array are called ELEMENTS
+            -- Arrays are ordered, each item has a fixed position
+            -- The position is known as the INDEX
+            -- First element in an array is index 0
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
+            int[] testScores = {95, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
+            Console.WriteLine("The elements for each array are:\n");
+            Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+            Console.WriteLine();
+            Console.WriteLine("testScore: \n" + String.Join(", ", testScores));
+            Console.WriteLine();
+            Console.WriteLine("GPA: \n" + String.Join(", ", GPA));
+            Console.WriteLine();
+
+        }
+    }
+}
