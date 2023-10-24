884130e0c3e205f4fadebdaccff071cf478735e2
A.O.Burx <amarionoburks@gmail.com>
2023-10-24 14:27:49 -0400

    Create methodsParameters.cs

---
+++ 04a_methodsParameters/methodsParameters.cs
@@ -0,0 +1,45 @@
+// Amarion Burks, Methods Parameters, v0.1
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        // METHOD: named block of code, can be used repeatedly
+        // ALL methods have a SIGNATURE that defines its name, parameters, and output
+        // Example Signature
+        static void MyMethod()
+        {
+            Console.WriteLine("I like chicken nuggets");
+        }
+        // static: This method belongs to the current class, is NOT an object
+        // void: This method has no return value
+        // method that doesnt have "void" MUST end with "return:"
+        static int DoubleUp()
+        {
+            int sum = 0;
+            Console.WriteLine("This method will double a number and return it.");
+            Console.WriteLine("Please enter a number on the next line.");
+            sum = System.Convert.ToInt32(Console.ReadLine());
+            
+            // Can use either
+            //sum = sum * 2;
+            //sum *= 2;
+            //sum = sum + sum;
+            //sum += sum;
+
+            sum *= 2;
+            Console.WriteLine(sum);
+
+            // Can also do math in return
+            // return sum *= 2;
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+            DoubleUp();
+        }
+    }
+}
